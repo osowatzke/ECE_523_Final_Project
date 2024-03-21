5339bf5c945f0fb64e1e40676203676eb0eca385
Owen <osowatzke@gmail.com>
2024-03-20 21:14:59 -0700

    Updating label creation

--- Test_Code/yolov8/train.py
+++ Test_Code/yolov8/train.py
@@ -14,28 +14,29 @@ import os
 import shutil
 
 # COPY DATA #########################################################
-path_this_file = os.path.split(os.path.abspath(__file__))[0]
-run_dir = os.path.join(path_this_file,'..','..','run')
-datasets_dir = os.path.join(run_dir,'datasets')
-data_dir = os.path.join(path_this_file,'..','..','FLIR_ADAS_v2')
-training_dir = os.path.join(data_dir, 'images_thermal_train','data')
-if not os.path.isdir(os.path.join(datasets_dir, 'training')):
-    os.makedirs(os.path.join(datasets_dir, 'training'))
-    shutil.copytree(training_dir, os.path.join(datasets_dir, 'training', 'images'))
-parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'training', 'labels'))
-parser.run()
-training_dir = os.path.join(data_dir, 'video_thermal_test','data')
-if not os.path.isdir(os.path.join(datasets_dir, 'test')):
-    os.makedirs(os.path.join(datasets_dir, 'test'))
-    shutil.copytree(training_dir, os.path.join(datasets_dir, 'test', 'images'))
-parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'test', 'labels'))
-parser.run()
-training_dir = os.path.join(data_dir, 'images_thermal_val','data')
-if not os.path.isdir(os.path.join(datasets_dir, 'valid')):
-    os.makedirs(os.path.join(datasets_dir, 'valid'))
-    shutil.copytree(training_dir, os.path.join(datasets_dir, 'valid', 'images'))
-parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'valid', 'labels'))
-parser.run()
+def setup_dir(sub_dir, force=False):
+    path_this_file = os.path.split(os.path.abspath(__file__))[0]
+    in_dir = os.path.join(path_this_file,'..','..','FLIR_ADAS_v2',sub_dir)
+    out_dir = os.path.join(path_this_file,'..','..','run','datasets')
+    out_dir = os.path.join(out_dir, sub_dir.split('_')[-1])
+    dir_exists = os.path.isdir(out_dir)
+    create_dir = not dir_exists or force
+    if create_dir:
+        if dir_exists:
+            shutil.rmtree(out_dir)
+        os.makedirs(out_dir)
+        in_image_dir = os.path.join(in_dir, 'data')
+        out_image_dir = os.path.join(out_dir, 'images')
+        shutil.copytree(in_image_dir, out_image_dir)
+        in_json_file = os.path.join(in_dir, 'index.json')
+        label_dir = os.path.join(out_dir, 'labels')
+        parser = JsonParser(in_json_file, label_dir)
+        parser.run()
+
+force_flag = True
+setup_dir('images_thermal_train', force_flag)
+setup_dir('images_thermal_val', force_flag)
+setup_dir('video_thermal_test', force_flag)
 
 # TRAINING ##########################################################
 
