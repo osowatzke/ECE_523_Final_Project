c400dab418b5fec3d4687938a6f892739b1408af
Owen <osowatzke@gmail.com>
2024-03-20 20:55:13 -0700

    Adding missing file

---
+++ Test_Code/yolov8/json_parser.py
@@ -0,0 +1,78 @@
+import json
+import shutil
+import os
+
+class JsonParser:
+
+    __LABELS = {
+        'person'        : 0,
+        'bike'          : 1,
+        'car'           : 2,
+        'motor'         : 3,
+        'bus'           : 4,
+        'train'         : 5,
+        'truck'         : 6,
+        'light'         : 7,
+        'hydrant'       : 8,
+        'sign'          : 9,
+        'dog'           : 10,
+        'deer'          : 11,
+        'skateboard'    : 12,
+        'stroller'      : 13,
+        'scooter'       : 14,
+        'other vehicle' : 15
+    }
+
+    def __init__(self, json_file, out_dir):
+        self.json_file = json_file    
+        self.out_dir = out_dir
+        self.json_data = None
+
+    def run(self):
+        self.create_out_dir()
+        self.read_json_file(self.json_file)
+        self.convert_to_yolo_format()
+
+    def create_out_dir(self):
+        if os.path.isdir(self.out_dir):
+            shutil.rmtree(self.out_dir)
+        os.makedirs(self.out_dir, exist_ok=False)
+
+    def read_json_file(self, json_file):
+        with open(json_file) as fid:
+            self.json_data = json.load(fid)
+
+    def convert_to_yolo_format(self):
+        frames = self.json_data['frames']
+        for frame in frames:
+            image_height = frame['height']
+            image_width = frame['width']
+            annotations = frame['annotations']
+            frame_index = frame['videoMetadata']['frameIndex']
+            video_id = frame['videoMetadata']['videoId']
+            frame_id = frame['datasetFrameId']
+            file_name = f'video-{video_id}-frame-{frame_index:>0{6}d}-{frame_id}.txt'
+            file_path = os.path.join(self.out_dir, file_name)
+            with open(file_path,'w') as file:
+                for annotation in annotations:
+                    h = annotation['boundingBox']['h']
+                    w = annotation['boundingBox']['w']
+                    x = annotation['boundingBox']['x']
+                    y = annotation['boundingBox']['y']
+                    x = x + w/2.0
+                    y = y + h/2.0
+                    h = h/image_height
+                    w = w/image_width
+                    x = x/image_width
+                    y = y/image_height
+                    for label in annotation['labels']:
+                        if label in self.__LABELS.keys():
+                            id = self.__LABELS[label]
+                            file.write(f'{id} {x:>.6f} {y:>.6f} {w:>.6f} {h:>.6f}\n')
+
+if __name__ == "__main__":
+    json_file = '../../FLIR_ADAS_v2/images_thermal_train/index.json'
+    out_dir = os.path.join(os.path.split(os.path.abspath(__file__))[0],'run','labels')
+    parser = JsonParser(json_file, out_dir)
+    parser.run()
+
