1a90eff178351e219dd8493dc99f3cf722132452
Owen <osowatzke@gmail.com>
2024-03-20 20:50:42 -0700

    Getting stuff to work

--- Test_Code/yolov8/train.py
+++ Test_Code/yolov8/train.py
@@ -9,7 +9,33 @@
 # DEPDENCIES ########################################################
 
 from ultralytics import YOLO
+from json_parser import JsonParser
+import os
+import shutil
 
+# COPY DATA #########################################################
+path_this_file = os.path.split(os.path.abspath(__file__))[0]
+run_dir = os.path.join(path_this_file,'..','..','run')
+datasets_dir = os.path.join(run_dir,'datasets')
+data_dir = os.path.join(path_this_file,'..','..','FLIR_ADAS_v2')
+training_dir = os.path.join(data_dir, 'images_thermal_train','data')
+if not os.path.isdir(os.path.join(datasets_dir, 'training')):
+    os.makedirs(os.path.join(datasets_dir, 'training'))
+    shutil.copytree(training_dir, os.path.join(datasets_dir, 'training', 'images'))
+parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'training', 'labels'))
+parser.run()
+training_dir = os.path.join(data_dir, 'video_thermal_test','data')
+if not os.path.isdir(os.path.join(datasets_dir, 'test')):
+    os.makedirs(os.path.join(datasets_dir, 'test'))
+    shutil.copytree(training_dir, os.path.join(datasets_dir, 'test', 'images'))
+parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'test', 'labels'))
+parser.run()
+training_dir = os.path.join(data_dir, 'images_thermal_val','data')
+if not os.path.isdir(os.path.join(datasets_dir, 'valid')):
+    os.makedirs(os.path.join(datasets_dir, 'valid'))
+    shutil.copytree(training_dir, os.path.join(datasets_dir, 'valid', 'images'))
+parser = JsonParser(os.path.join(training_dir,'..','index.json'),  os.path.join(datasets_dir, 'valid', 'labels'))
+parser.run()
 
 # TRAINING ##########################################################
 
