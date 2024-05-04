4b3bfd07deee4516fc521c8e54a392f856017444
Owen <osowatzke@gmail.com>
2024-05-04 04:51:02 -0700

    Pointing tensorboard to run directory

--- src/NetworkTrainer.py
+++ src/NetworkTrainer.py
@@ -202,9 +202,10 @@ class NetworkTrainer:
     def train(self):
 
         # Create TensorBoard SummaryWriter instance
-        log_dir = os.path.join('/tmp/runs',os.path.basename(self.run_dir))
-        #log_dir = os.path.dirname(__file__)
-        #log_dir = os.path.join(log_dir,os.path.basename(self.run_dir))
+        # log_dir = os.path.join('/tmp/runs',os.path.basename(self.run_dir))
+        log_dir = os.path.dirname(__file__)
+        log_dir = os.path.join(log_dir,os.path.basename(self.run_dir))
+
         writer = SummaryWriter(log_dir)
 
         # Save the initial random generator state
