b00b900f92220a84a2da527f12c00f010d66f8f2
Owen <osowatzke@gmail.com>
2024-05-05 06:39:42 -0700

    Trying to run four stage Faster RCNN

--- src/runNetwork.py
+++ src/runNetwork.py
@@ -8,6 +8,7 @@ from FlirDataset import FlirDataset
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from fasterRCNN import FasterRCNN
+from scipy.io import savemat, loadmat
 #import matplotlib.patches.BoxStyle as BoxStyle
 import os
 
@@ -25,33 +26,50 @@ data_dir = data_manager.get_download_dir()
 PathConstants(data_dir)
 
 # Create dataset object
-train_data = FlirDataset(PathConstants.TRAIN_DIR, downsample=1, num_images=100, device=device)
+train_data = FlirDataset(PathConstants.TRAIN_DIR, compute_mean_std=False, num_images=100, downsample=1, device=device) # num_images=100, 
+# savemat('train_data_statistics.mat',{'image_std' : train_data.std, 'image_mean' : train_data.mean})
+
+mat_dict = loadmat('train_data_statistics.mat')
+image_mean = mat_dict['image_mean']
+image_std = mat_dict['image_std']
+
+image_mean = torch.tensor(image_mean, dtype=torch.float32)
+image_std = torch.tensor(image_std, dtype=torch.float32)
+image_mean = image_mean.ravel()
+image_std = image_std.ravel()
 
 # Create a faster RCNN model
-model = FasterRCNN(train_data[0][0].shape)
+# model = FasterRCNN(train_data[0][0].shape)
+image_size = train_data[0][0].shape
+model = FasterRCNN(
+    image_size       = image_size,
+    normalize_images = True,
+    image_mean       = image_mean,
+    image_std        = image_std)
 model.to(device)
 
 # Load pretrained weights
 file_path = os.path.dirname(__file__)
-weights_path = os.path.join(file_path,'weights','cp__epoch_50_batch_0.pth')
+weights_path = os.path.join(file_path,'weights','normalized','cp__epoch_25_batch_0.pth')
 state_dict = torch.load(weights_path,map_location=device)
 model.load_state_dict(state_dict['model_state'])
+
 
 # Run model
 model.eval()
 #print(type(train_data[0][0]))
 #test_data = train_data[0][0].reshape((1,) + train_data[0][0].shape)
 #print(test_data.shape)
-img = train_data[49][0]
-pred = model(img.reshape((1,) + img.shape),[train_data[49][1]])[0]
+img = train_data[0][0]
+pred = model(img.reshape((1,) + img.shape),[train_data[0][1]])[0]
 img = img[0,:,:].detach().numpy()
 #print(test_data[0,:,:].shape)
 #print(pred)
 boxes = pred[0]['boxes'].detach().numpy()
 labels = pred[0]['labels'].detach().numpy()
 scores = pred[0]['scores'].detach().numpy()
-boxes = boxes[scores > 0.1]
-labels = labels[scores > 0.1]
+boxes = boxes[scores > 0.3]
+labels = labels[scores > 0.3]
 boxes = boxes[labels != 0]
 labels = labels[labels != 0]
 #print(boxes)
