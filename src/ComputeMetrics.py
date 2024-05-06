55b3d4a9ed46a42e8c27e314fab2b7dfb6936f7c
Nicolas Blanchard <nickyblanch@email.arizona.edu>
2024-05-05 18:27:13 -0700

    compute metrics

--- src/ComputeMetrics.py
+++ src/ComputeMetrics.py
@@ -8,6 +8,7 @@ import matplotlib.patches as patches
 from fasterRCNN import FasterRCNN
 from fasterRCNN import rcnn_collate_fn
 import time
+import argparse
 
 def collate_fn(data):
     """
@@ -119,6 +120,13 @@ if __name__ == "__main__":
     from FlirDataset import FlirDataset
     import os
 
+    # Parse optional input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--class_options', default=1, type=int)
+    parser.add_argument('-n', '--num_images', default=50, type=int)
+    parser.add_argument('-m', '--map_vals', nargs=3, default=[0.25,0.25,0.25], type=int)
+    args = parser.parse_args()
+
     # Determine the device
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
@@ -149,11 +157,11 @@ if __name__ == "__main__":
     data_dir = data_manager.get_download_dir()
     PathConstants(data_dir)
 
-    if True:
+    if False:
 
         # Create dataset object
         # train_data = FlirDataset(PathConstants.TRAIN_DIR, downsample=1, num_images=-1, device=device)
-        valid_data = FlirDataset(PathConstants.VAL_DIR, downsample=1, num_images=100, device=device)
+        valid_data = FlirDataset(PathConstants.VAL_DIR, downsample=1, num_images=args.num_images, device=device)
 
         # Get reference bounding boxes
         targets = get_targets(valid_data)
@@ -174,15 +182,26 @@ if __name__ == "__main__":
         imgs = model_dict['imgs']
 
     for min_score in range(10):
+
         plotted_image_index = 34
-        desired_classes = (3,)
+
+        if args.class_options == 1:
+            desired_classes = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        elif args.class_options == 2:
+            desired_classes = (3,1)
+        elif args.class_options == 3:
+            desired_classes = (3,)
+        elif args.class_options == 4:
+            desired_classes = (1,)
+
         # desired_classes = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
         filtered_predictions = filter_predictions(predictions, min_score=(0.05*min_score), classes=desired_classes)
         filtered_targets = filter_targets(targets, classes=desired_classes)
 
         iou = get_iou(filtered_predictions, filtered_targets)
 
-        map = get_map(filtered_predictions, filtered_targets, [0.25])
+        map = get_map(filtered_predictions, filtered_targets, args.map_vals)
+        # map = get_map(filtered_predictions, filtered_targets, [0.25, .25, .25])
 
         # img = model_dict['imgs'][plotted_image_index][0]
         # img_data_all = np.uint8(img.permute(1, 2, 0).numpy())
